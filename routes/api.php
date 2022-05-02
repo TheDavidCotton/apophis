f9f732f631abe133379dbc8ca66788ae3d8919b5
David Cotton <49660345+TheDavidCotton@users.noreply.github.com>
2022-05-02 16:12:14 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/apophis', function (Request $request) {
     //header('Content-Type: application/json');
 
-    $flyby = Carbon::createFromFormat('YYYY-MM-DD', env('ENCOUNTER'))->format('Y-m-d');
+    $flyby = Carbon::createFromFormat('m/d/Y', env('ENCOUNTER'))->format('Y-m-d');
 
     return $flyby;
     //return response()
