8d0a320edb239a00a08101b7275691c9caa67bb8
David Cotton <49660345+TheDavidCotton@users.noreply.github.com>
2022-05-02 15:03:15 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/api', function (Request $request) {
+    return "You are going to die";
 });
