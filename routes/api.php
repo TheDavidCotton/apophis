91bafb70a1be5832f45f8560079cf75ad2f019f8
David Cotton <49660345+TheDavidCotton@users.noreply.github.com>
2022-05-02 16:21:08 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,25 @@ Route::get('/apophis', function (Request $request) {
     //header('Content-Type: application/json');
 
     $flyby = Carbon::createFromFormat('Y-m-d', env('ENCOUNTER'));
-    $date = Carbon::now()->diffInDays($flyby, false);
-    return $date;
+
+    $diffArray = array();
+
+    $date_diff = Carbon::now()->diffInDays($flyby, false);
+    array_push($diff, $date_diff . 'days');
+
+    $hours_diff = Carbon::now()->diffInHours($flyby, false);
+    array_push($diff, $hours_diff  . 'hours');
+
+    $Minutesdiff = Carbon::now()->diffInMinutes($flyby, false);
+    array_push($diff, $Minutesdiff  . 'minutes');
+
+    $seconddiff = Carbon::now()->DiffInSeconds($flyby, false);
+    array_push($diff, $seconddiff  . 'seconds');
+
+    $k = array_rand($diffArray);
+    $diff = $diffArray[$k];
+
+    return $diff;
     //return response()
             //->json(['blurb' => $blurb, 'date' => $date]);
 
