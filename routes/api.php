562c23d36bf108eb0201b5f392fb2642b6b65ebd
David Cotton <49660345+TheDavidCotton@users.noreply.github.com>
2022-05-02 15:20:38 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/apophis', function (Request $request) {
+    header('Content-Type: application/json');
     return "You are going to die";
 });
